2c7b123eb7856779d793931c75faf7783778afa4
JupiterMouse <explorren@gmail.com>
2018-05-03 17:54:05 +0800

    Kafka生产者生产数据

---
+++ src/main/java/com/message/ProducerMessage.java
@@ -0,0 +1,30 @@
+package com.message;
+
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: RenQiQiang
+ * @Date: 2018-05-03 17:34
+ * @Desciption:
+ */
+
+@Component
+@Slf4j
+public class ProducerMessage {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+
+    public void send(String topic,Integer partition,String key, String value) {
+
+        log.info("sending key='{}' to valye='{}'", key,value);
+
+        kafkaTemplate.send("gas",0, "message",value);// "message"
+    }
+}
