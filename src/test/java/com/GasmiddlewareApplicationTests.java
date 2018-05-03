cb540064b800b8f9a5d83f9da709b25b124bad23
JupiterMouse <explorren@gmail.com>
2018-05-03 20:03:12 +0800

    可运行

--- src/test/java/com/GasmiddlewareApplicationTests.java
+++ src/test/java/com/GasmiddlewareApplicationTests.java
@@ -1,7 +1,9 @@
 package com;
 
+import com.message.ProducerMessage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GasmiddlewareApplicationTests {
 
+    @Autowired
+    ProducerMessage producerMessage;
+
+    @Autowired
+
+
     @Test
     public void contextLoads() {
+        producerMessage.send("test-gas",0,"message","test");
     }
 
 }
