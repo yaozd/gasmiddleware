b5784e154678efc586b37a553c3ee1ca6866f4c5
JupiterMouse <explorren@gmail.com>
2018-05-03 15:56:36 +0800

    Kafka的消费者配置

---
+++ src/main/java/com/config/ConsumerConfig.java
@@ -0,0 +1,66 @@
+package com.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: RenQiQiang
+ * @Date: 2018-04-25 18:10
+ * @Desciption:
+ */
+
+
+@Configuration
+@EnableKafka
+public class ConsumerConfig {
+
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${kafka.consumer.group-id}")
+    private String consumerGroupId;
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId);
+        // maximum records per poll
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "3");
+//        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+
+
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        // enable batch listening
+        factory.setBatchListener(true);
+
+        return factory;
+    }
+
+}
